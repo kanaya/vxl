a0198c1696cf0359e97c6f527eb1315887383dd0
Ichi Kanaya <kanaya@pineapple.cc>
2024-05-17 22:10:53 +0900

    writing...

--- src/xyz-to-vxl.py
+++ src/xyz-to-vxl.py
@@ -7,17 +7,20 @@ from tqdm import tqdm
 parser = argparse.ArgumentParser()
 parser.add_argument('xyz_file', help='XYZ file (input)')
 parser.add_argument('vxl_file', help='VXL file (output)')
-parser.add_argument('-r', '--resolution', type=int, default=511, help='Maximum resolution (default: 511)')
+parser.add_argument('-r', '--resolution', type=int, default=512, help='Maximum resolution (default: 512)')
 args = parser.parse_args()
 
 xyz_filename = args.xyz_file
 vxl_filename = args.vxl_file
 resolution = args.resolution
 
-def p_to_q(p, mins, d_max):
+def p_to_q(p, mins, d_max, dimensions):
 	x = round((p[0] - mins[0]) / d_max * resolution)
 	y = round((p[1] - mins[1]) / d_max * resolution)
 	z = round((p[2] - mins[2]) / d_max * resolution)
+	x = min(x, dimensions[0] - 1)
+	y = min(y, dimensions[1] - 1)
+	z = min(z, dimensions[2] - 1)
 	return [x, y, z]
 
 def create_voxel(xyz_list):
@@ -33,12 +36,11 @@ def create_voxel(xyz_list):
 	diffs = [x_max - x_min, y_max - y_min, z_max - z_min]
 	d_max = max(diffs)
 	voxel_size = d_max / resolution
-	dimensions = (math.ceil(diffs[0] / voxel_size) + 1, math.ceil(diffs[1] / voxel_size) + 1, math.ceil(diffs[2] / voxel_size) + 1)
-	print('dimensions = {}'.format(dimensions))
+	dimensions = (math.ceil(diffs[0] / voxel_size), math.ceil(diffs[1] / voxel_size), math.ceil(diffs[2] / voxel_size))
 	voxel = np.zeros(dimensions, dtype=int)
 	p_array = xyz_array.reshape([n, 3])
 	for p in tqdm(p_array):
-		q = p_to_q(p, mins, d_max)
+		q = p_to_q(p, mins, d_max, dimensions)
 		voxel[q[0], q[1], q[2]] += 1
 	return voxel
 
